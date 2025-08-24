f52c86a9c4e92ef80bec910d3d01692f0b589da2
ghorbandrohini <ghorbandrohini@gmail.com>
2025-08-24 20:23:08 +0530

    first commit

---
+++ Sum.java
@@ -0,0 +1,11 @@
+public class Sum 
+{
+    public static void main(String[] args)
+    {
+        int num1 = 4;
+        int num2 = 10;
+        int result;
+        result = num1 + num2;
+        System.out.println(result);
+    }
+}
